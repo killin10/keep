7df107210187e560e53c5fabc4eec5f81238854d
Shi Wei <shiwei2012@gmail.com>
2017-08-24 19:15:05 +0800

    add function comment to test YCM GoTo

--- cmake/tutorial/MathFunctions/MathFunctions.h
+++ cmake/tutorial/MathFunctions/MathFunctions.h
@@ -1,5 +1,12 @@
 #ifndef MATH_FUNCTIONS_H
 
+/**
+ * my custrom sqrt() implementation
+ *
+ * @param val [in] value to be processed
+ *
+ * @return sqrt value
+ */
 double mysqrt(double val);
 
 #endif  // MATH_FUNCTIONS_H
