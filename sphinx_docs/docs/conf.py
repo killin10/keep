0d7f15e2745d78d75987dcf273b81fed4bd7273d
Shi Wei <shiwei2012@gmail.com>
2017-10-09 17:19:42 +0800

    use theme 'sphinx_rtd_theme'

--- sphinx_docs/docs/conf.py
+++ sphinx_docs/docs/conf.py
@@ -4,7 +4,8 @@ extensions = ['breathe']
 # The master toctree document.
 master_doc = 'index'
 
-html_theme = 'sphinxdoc'
+# html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
 
 breathe_projects = { "SphinxDocsDemo": "doxy_xml/" }
 breathe_default_project = "SphinxDocsDemo"
