fb417f179f70f232d150fa5c22636b34d5201852
Shi Wei <shiwei2012@gmail.com>
2017-11-01 23:22:43 +0800

    add comments for auto demo

--- cpp11/auto.cc
+++ cpp11/auto.cc
@@ -11,11 +11,16 @@ int main(void) {
 	int i = 0, &r = i;
 	const int ci = i, &cr = ci;
 
+	// a is int
 	auto a = r;
+	// b is int
 	auto b = ci;
 	b = 3;
+	// c is const int *
 	auto c = &ci;
+	// d is const ref
 	auto &d = ci;
+	// e is const ref
 	const auto &e = 42;
 
 	return 0;
