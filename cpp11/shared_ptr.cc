0ade1b2d933fa885c5fce84a0ed6d669578cef49
Shi Wei <shiwei2012@gmail.com>
2017-11-09 10:13:31 +0800

    shared_ptr for dynamic array

--- cpp11/shared_ptr.cc
+++ cpp11/shared_ptr.cc
@@ -13,49 +13,46 @@ void f1(std::shared_ptr<D> p);
 void f2(std::shared_ptr<D> p);
 
 class D {
-	public:
-		D() {
-			std::cout << "in D()" << std::endl;
-		}
+ public:
+  D() { std::cout << "in D()" << std::endl; }
 
-		D(int i): i_(i) {
-			std::cout << "in D(int)" << std::endl;
-		}
+  D(int i) : i_(i) { std::cout << "in D(int)" << std::endl; }
 
-		~D() {
-			std::cout << "in ~D()" << std::endl;
-		}
+  ~D() { std::cout << "in ~D()" << std::endl; }
 
-		int i() {
-			return i_;
-		}
+  int i() { return i_; }
 
-	private:
-		int i_ = 1;
+ private:
+  int i_ = 1;
 };
 
 void f1(std::shared_ptr<D> p) {
-	std::cout << "f1() " << p->i() << std::endl;
-	f2(p);
+  std::cout << "f1() " << p->i() << std::endl;
+  f2(p);
 }
 
-void f2(std::shared_ptr<D> p) {
-	std::cout << "f2() " << p->i() << std::endl;
-}
+void f2(std::shared_ptr<D> p) { std::cout << "f2() " << p->i() << std::endl; }
 
 void f3() {
-	// use shared_ptr to mamange raw pointer
-	D *p = new D();
-	std::shared_ptr<D> sp(p);
+  // use shared_ptr to mamange raw pointer
+  D *p = new D();
+  std::shared_ptr<D> sp(p);
 
-	// do biz things & p will be deleted after return
+  // do biz things & p will be deleted after return
+}
+
+void f4() {
+  // use shared_ptr to manage dynamic array
+
+  std::shared_ptr<D> p = std::shared_ptr<D>(new D[3], [](D *p) { delete[] p; });
 }
 
 int main(void) {
-	f1(std::make_shared<D>(2));
+  f1(std::make_shared<D>(2));
 
-	f3();
+  f3();
+	f4();
 
-	std::cout << "before exit" << std::endl;
-	return 0;
+  std::cout << "before exit" << std::endl;
+  return 0;
 }
