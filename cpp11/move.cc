ee0a72e769e75a3a6f48053c6d91c247dd49f7c4
Shi Wei <shiwei2012@gmail.com>
2017-11-15 19:42:06 +0800

    cpp11: make_move_iterator()

--- cpp11/move.cc
+++ cpp11/move.cc
@@ -6,7 +6,9 @@
  */
 
 #include <iostream>
+#include <string>
 #include <utility>
+#include <vector>
 
 struct C {
   C() { std::cout << "ctor" << std::endl; }
@@ -27,6 +29,17 @@ int main(void) {
   C c;
   C d = c;
   C e = std::move(c);
+
+  std::vector<std::string> orignal{"one", "two", "three"};
+  std::vector<std::string> dest(std::make_move_iterator(orignal.begin()),
+                                std::make_move_iterator(orignal.end()));
+
+  std::for_each(orignal.begin(), orignal.end(),
+                [](const std::string &s) { std::cout << s << "\t"; });
+  std::cout << std::endl;
+  std::for_each(dest.begin(), dest.end(),
+                [](const std::string &s) { std::cout << s << "\t"; });
+  std::cout << std::endl;
 
   return 0;
 }
