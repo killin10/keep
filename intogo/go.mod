5a560fc21917c3e5a0acac07d77869ab179b9e74
limu.sw <limu.sw@alibaba-inc.com>
2020-02-15 19:16:27 +0800

    go-cmp demo

--- intogo/go.mod
+++ intogo/go.mod
@@ -3,6 +3,7 @@ module github.com/killin10/keep/intogo
 go 1.13
 
 require (
+	github.com/google/go-cmp v0.4.0
 	github.com/stretchr/testify v1.4.0
 	golang.org/x/tour v0.0.0-20200201212631-8f38c9a8d074
 )
--- intogo/howto/hello/hello.go
+++ intogo/howto/hello/hello.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/killin10/keep/intogo/howto/stringutil"
 )
 
 func main() {
-	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
