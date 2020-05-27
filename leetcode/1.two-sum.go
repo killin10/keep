c463167128fc2cda084743e9dc8c4c495fb1d629
limu.sw <limu.sw@alibaba-inc.com>
2020-05-27 23:02:14 +0800

    add package statement

--- leetcode/1.two-sum.go
+++ leetcode/1.two-sum.go
@@ -15,25 +15,27 @@
  *
  * Given an array of integers, return indices of the two numbers such that they
  * add up to a specific target.
- * 
+ *
  * You may assume that each input would have exactly one solution, and you may
  * not use the same element twice.
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Given nums = [2, 7, 11, 15], target = 9,
- * 
+ *
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
+// package leetcode
+
 func twoSum(nums []int, target int) []int {
-    numToIndex := make(map[int]int)
-	
+	numToIndex := make(map[int]int)
+
 	for i, v := range nums {
 		complement := target - v
 		ci, ok := numToIndex[complement]
@@ -44,5 +46,5 @@ func twoSum(nums []int, target int) []int {
 	}
 	return []int{-1, -1}
 }
-// @lc code=end
 
+// @lc code=end
