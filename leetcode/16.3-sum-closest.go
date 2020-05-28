bcf6740a8f0ae2abdc86e1d184c9fa1b95558841
limu.sw <limu.sw@alibaba-inc.com>
2020-05-28 11:08:17 +0800

    16.3-sum-closest

---
+++ leetcode/16.3-sum-closest.go
@@ -0,0 +1,74 @@
+/*
+ * @lc app=leetcode id=16 lang=golang
+ *
+ * [16] 3Sum Closest
+ *
+ * https://leetcode.com/problems/3sum-closest/description/
+ *
+ * algorithms
+ * Medium (45.81%)
+ * Likes:    1910
+ * Dislikes: 135
+ * Total Accepted:    454.8K
+ * Total Submissions: 992.8K
+ * Testcase Example:  '[-1,2,1,-4]\n1'
+ *
+ * Given an array nums of n integers and an integer target, find three integers
+ * in nums such that the sum is closest to target. Return the sum of the three
+ * integers. You may assume that each input would have exactly one solution.
+ *
+ * Example:
+ *
+ *
+ * Given array nums = [-1, 2, 1, -4], and target = 1.
+ *
+ * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ *
+ *
+ */
+
+// @lc code=start
+// package leetcode
+
+import (
+	"math"
+	"sort"
+)
+
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+
+	closest := math.MaxInt32
+	for i := 0; i < len(nums)-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		left, right := i+1, len(nums)-1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+
+			if abs(closest-target) > abs(sum-target) {
+				closest = sum
+			}
+			if sum == target {
+				return target
+			} else if sum > target {
+				right--
+			} else {
+				left++
+			}
+		}
+	}
+
+	return closest
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+// @lc code=end
