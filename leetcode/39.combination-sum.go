b946c9d5ff58dc034958b80b40e87da0cffc7e5a
limu.sw <limu.sw@alibaba-inc.com>
2020-05-28 14:06:41 +0800

    39.combination-sum

---
+++ leetcode/39.combination-sum.go
@@ -0,0 +1,91 @@
+/*
+ * @lc app=leetcode id=39 lang=golang
+ *
+ * [39] Combination Sum
+ *
+ * https://leetcode.com/problems/combination-sum/description/
+ *
+ * algorithms
+ * Medium (54.72%)
+ * Likes:    3491
+ * Dislikes: 108
+ * Total Accepted:    514K
+ * Total Submissions: 937.5K
+ * Testcase Example:  '[2,3,6,7]\n7'
+ *
+ * Given a set of candidate numbers (candidates) (without duplicates) and a
+ * target number (target), find all unique combinations in candidates where the
+ * candidate numbers sums to target.
+ *
+ * The same repeated number may be chosen from candidates unlimited number of
+ * times.
+ *
+ * Note:
+ *
+ *
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: candidates = [2,3,6,7], target = 7,
+ * A solution set is:
+ * [
+ * ⁠ [7],
+ * ⁠ [2,2,3]
+ * ]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: candidates = [2,3,5], target = 8,
+ * A solution set is:
+ * [
+ * [2,2,2,2],
+ * [2,3,3],
+ * [3,5]
+ * ]
+ *
+ *
+ */
+
+// @lc code=start
+// package leetcode
+
+import (
+	"math"
+	"sort"
+)
+
+func combinationSum(candidates []int, target int) [][]int {
+	sort.Ints(candidates)
+	return doCombinationSum(candidates, target, math.MinInt32)
+}
+
+func doCombinationSum(candidates []int, target int, lastChoice int) [][]int {
+	result := [][]int{}
+
+	for _, v := range candidates {
+		if v < lastChoice {
+			continue
+		}
+
+		if v == target {
+			result = append(result, []int{v})
+		} else if v < target {
+			subResult := doCombinationSum(candidates, target-v, v)
+			for _, sr := range subResult {
+				result = append(result, append([]int{v}, sr...))
+			}
+		} else {
+			break
+		}
+	}
+
+	return result
+}
+
+// @lc code=end
